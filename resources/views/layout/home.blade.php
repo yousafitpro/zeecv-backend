9b9394443107a2075079534901b85fd42c801498
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-08 17:30:37 +0500

    zzz

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/favicon/apple-touch-icon.png')}}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/favicon/favicon-32x32.png')}}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/favicon/favicon-16x16.png')}}">
+  <link rel="manifest" href="{{asset('assets/favicon/site.webmanifest')}}">
   <title>ZeeCV · AI CV Builder</title>
   <!-- Font Awesome (free icons) -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
