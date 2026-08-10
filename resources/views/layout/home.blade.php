e304ba67ae2391babb53e52e2a90108001f8d0fc
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-10 15:19:23 +0500

    Update home.blade.php

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -54,7 +54,15 @@
 
   @include('home.css.style')
 @include("layout.includes.css")
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GQYT8LDL91"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-GQYT8LDL91');
+</script>
 </head>
 <body>
 <div style="height: 100px"></div>
