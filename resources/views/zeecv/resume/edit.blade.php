1c153adfdf06355cc1451083cd95772400bc3d2f
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-12 23:52:10 +0500

    Update edit.blade.php

--- resources/views/zeecv/resume/edit.blade.php
+++ resources/views/zeecv/resume/edit.blade.php
@@ -45,7 +45,7 @@
         <a  href="{{ route('resume.pdf.preview',request('id')) }}?resume={{ now() }}&resume_token={{ auth()->user()->login_token }}" target="_blank" class="btn btn-save btn-preview px-4 rounded-pill" >PDF Preview</a>
       @endif
       
-    <a href="{{ route('resume.pdf',request('id')) }}?resume={{ now() }}&resume_token={{ auth()->user()->login_token }}" class="btn btn-save px-4 rounded-pill" >Download</a>
+    <a href="{{ route('resume.pdf',request('id')) }}?resume={{ now() }}&resume_token={{ auth()->user()->login_token }}" class="btn btn-save px-4 rounded-pill" >Download PDF</a>
   
   </div>
   </header>
