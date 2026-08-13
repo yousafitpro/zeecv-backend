cd424c73c4fad6e8f2b3d0298ae3ab9446cbd731
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-13 10:44:56 +0500

    Update edit.blade.php

--- resources/views/zeecv/resume/edit.blade.php
+++ resources/views/zeecv/resume/edit.blade.php
@@ -36,7 +36,7 @@
 <body>
 
   <!-- Top Navigation Bar -->
-  <header class="app-header sticky-top d-flex justify-content-between align-items-center px-4 py-2 bg-white">
+  <header class="app-header sticky-top {{session('is_app','no')=='yes'?'':'d-flex justify-content-between align-items-center'}} px-4 py-2 bg-white">
     @if (session('is_app','no')!='yes')
     <a href="{{ url('dashboard') }}" class="btn btn-link text-secondary text-decoration-none p-0" style="float:right">
       <i class="fas fa-arrow-left mr-2"></i>Dashboard
