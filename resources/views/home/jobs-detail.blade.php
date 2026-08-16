3ce61c10e078a587965349c3b58ae632413d319b
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-16 12:48:10 +0500

    zzz

--- resources/views/home/jobs-detail.blade.php
+++ resources/views/home/jobs-detail.blade.php
@@ -1189,7 +1189,7 @@
 
                     <div class="job_container_outer_description">
 
-                       {!! html_entity_decode($job->description) !!}
+                       {!! clean(html_entity_decode($job->description)) !!}
 
 
                     </div>
