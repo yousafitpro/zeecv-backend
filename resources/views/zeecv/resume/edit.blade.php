398ac7095bfbaae3e513591f91b2225aa380793b
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-18 15:45:16 +0500

    zz

--- resources/views/zeecv/resume/edit.blade.php
+++ resources/views/zeecv/resume/edit.blade.php
@@ -37,13 +37,13 @@
 
   <!-- Top Navigation Bar -->
   <header class="app-header sticky-top d-flex justify-content-between align-items-center' px-4 py-2 bg-white">
-    @if (session('is_app','no')!='yes')
+    {{-- @if (session('is_app','no')!='yes') --}}
     <a href="{{ route('home.jobs') }}" class="btn btn-link text-secondary text-decoration-none p-0">
       <i class="fas fa-arrow-left mr-2"></i>Jobs
     </a>
-    @else
+    {{-- @else
     <div>Edit Resume</div>
-    @endif
+    @endif --}}
     <div class="resume-edit-action-buttons">
       @if (session('is_app','no')!='yes')
         <a  onclick="resumePrintFunNewWindow('{{ route('resume.pdf.preview',request('id')) }}?resume={{ now() }}&resume_token={{ auth()->user()->login_token }}')" target="_blank" class="btn btn-save btn-preview px-4 rounded-pill" >PDF Preview</a>
