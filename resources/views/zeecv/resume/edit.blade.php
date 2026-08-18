d640ea6c05f52cd92b1fbc4a2b3e36e50540e8c6
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-18 11:56:28 +0500

    Update edit.blade.php

--- resources/views/zeecv/resume/edit.blade.php
+++ resources/views/zeecv/resume/edit.blade.php
@@ -39,7 +39,7 @@
   <header class="app-header sticky-top d-flex justify-content-between align-items-center' px-4 py-2 bg-white">
     @if (session('is_app','no')!='yes')
     <a href="{{ route('home.jobs') }}" class="btn btn-link text-secondary text-decoration-none p-0">
-      <i class="fas fa-arrow-left mr-2"></i>Dashboard
+      <i class="fas fa-arrow-left mr-2"></i>Jobs
     </a>
     @else
     <div>Edit Resume</div>
