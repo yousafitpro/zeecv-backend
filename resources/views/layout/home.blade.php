f357f97087db94c7cb27c9600d314fa3907d05df
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-19 14:23:41 +0500

    zzzzz

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -62,7 +62,7 @@
   @yield('meta_tags')
  <link rel="canonical"
           href="{{ url()->current() }}">
-  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/favicon/apple-touch-icon.png')}}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/favicon/apple-icon.png')}}">
   <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/favicon/favicon-32x32.png')}}">
   <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/favicon/favicon-16x16.png')}}">
   <link rel="manifest" href="{{asset('assets/favicon/site.webmanifest')}}">
--- resources/views/layout/includes/css.blade.php
+++ resources/views/layout/includes/css.blade.php
@@ -1,8 +1,5 @@
 
-  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/favicon/apple-touch-icon.png')}}">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/favicon/favicon-32x32.png')}}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/favicon/favicon-16x16.png')}}">
-  <link rel="manifest" href="{{asset('assets/favicon/site.webmanifest')}}">
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
