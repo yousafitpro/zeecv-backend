8c743231e1d52ecf3a4406424b801461205f2799
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-21 11:03:00 +0500

    Update JobsController.php

--- app/Http/Controllers/Job/JobsController.php
+++ app/Http/Controllers/Job/JobsController.php
@@ -266,7 +266,7 @@ public function queryProcess(Request $request)
         ->withQueryString();
         return view('home.ajax.jobs-list',$data);
         }else{
-          $data['list']=$data['list']->take(10)->get() ; 
+          $data['list']=$data['list']->take(20)->get() ; 
           return response()->json($data);
         }
         
