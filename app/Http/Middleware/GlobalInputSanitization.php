e6f7a693d3ff3b0e382b86b13a4b1732a9c253a2
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-30 16:29:40 +0500

    Update GlobalInputSanitization.php

--- app/Http/Middleware/GlobalInputSanitization.php
+++ app/Http/Middleware/GlobalInputSanitization.php
@@ -53,6 +53,7 @@ class GlobalInputSanitization
     protected $excludeFields = [
         '_token',
         '_method',
+        'desired_job_title',
         'password',
         'description',
         'long_description',
@@ -147,7 +148,7 @@ class GlobalInputSanitization
                 break;
             case 'title':
                 // Allow letters, numbers, spaces, and basic punctuation
-                $value = preg_replace('/[^a-zA-Z0-9\s\-\.\,\!\?\'\(\)]/', '', $value);
+                $value = preg_replace('/[^a-zA-Z0-9\s\-\.\,\!\?\&\'\(\)]/', '', $value);
                 $value = substr($value, 0, 200);
                 break;
         }
