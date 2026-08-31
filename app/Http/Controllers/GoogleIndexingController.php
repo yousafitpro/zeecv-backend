fa7f0a646cf94ca6d68aa70d75348bbf22c1185a
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-31 19:43:53 +0500

    Update GoogleIndexingController.php

--- app/Http/Controllers/GoogleIndexingController.php
+++ app/Http/Controllers/GoogleIndexingController.php
@@ -22,7 +22,6 @@ class GoogleIndexingController extends Controller
         $todaySent = JobCareer::where('sent_for_indexing_google', 1)
         ->whereDate('updated_at', Carbon::today())  // or 'sent_at' if you have that column
         ->count();
-        dd($todaySent);
         // Define batch size and time window (e.g., last 48 hours)
         $limit = 5;
         $hours = 1; // change as needed
