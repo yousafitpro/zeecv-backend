cb2e0f5f3c45945a84389b25451704231afd0e2b
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-02 16:14:38 +0500

    Update navbar.blade.php

--- resources/views/layout/includes/navbar.blade.php
+++ resources/views/layout/includes/navbar.blade.php
@@ -564,6 +564,7 @@ document.addEventListener('DOMContentLoaded', function () {
     <a class="navbar-brand py-0" href="{{ url('/') }}">
       <img src="{{ asset('app-icons/logo.png') }}" class="brand-logo" alt="ZeeCV Logo">
     </a>
+     <a href="{{ url('signup') }}" class="btn-nav-primary d-block d-md-none">Sign up</a>
 
     <!-- Mobile Toggler Button -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" 
