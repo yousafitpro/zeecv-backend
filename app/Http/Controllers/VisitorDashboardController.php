15c7d8a6c5f47c0145d998db7872dcfd79ed0f52
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-07 22:11:43 +0500

    Update VisitorDashboardController.php

--- app/Http/Controllers/VisitorDashboardController.php
+++ app/Http/Controllers/VisitorDashboardController.php
@@ -58,7 +58,7 @@ class VisitorDashboardController extends Controller
                         ->get();
         $data['top_clicked_jobs'] = (clone $visitQuery)->whereNotNull('job_slug')
                         ->select('job_slug', DB::raw('count(*) as count'))
-                        ->groupBy('job_slug')
+                        ->groupBy('count')
                         ->take(20)
                         ->get();
         $data['job_sources'] = (clone $jobQuery)->whereNotNull('source')
