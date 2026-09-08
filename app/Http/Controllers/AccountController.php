249bbcbfc710022a968a54e297e008adbf56f4fe
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-08 12:15:05 +0500

    Update AccountController.php

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -12,8 +12,14 @@ class AccountController extends Controller
     public function index()
     {
         $data['invoices']=Payment::where('user_id',auth_user_id())->get();
-        $data['subscription']=my_subscription()['sub'];
-        $data['is_expired']=my_subscription()['is_expired'];
+        if(!empty(my_subscription())){
+            $data['subscription']=my_subscription()['sub'];
+            $data['is_expired']=my_subscription()['is_expired'];
+        }else{
+            $data['subscription']=null;
+            $data['is_expired']=true;
+        }
+
         return view('account.index',$data);
     }
 }
