559835bdedd2f944ee24a1f500bbed58f5e351c0
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-08 10:56:41 +0500

    zz

---
+++ resources/views/packages/wait_payment_in_progress.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.home')
+
+@section('content')
+<h1>Please wait payment is in progress for status of subscription visit link below</h1>
+<a  href="{{ route('account.index') }}"></a>
+@endpush
