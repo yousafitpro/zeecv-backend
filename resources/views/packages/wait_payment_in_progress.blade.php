36b37276b020b06657ea913e9ba3f6d9bed69ebd
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-08 11:02:12 +0500

    Update wait_payment_in_progress.blade.php

--- resources/views/packages/wait_payment_in_progress.blade.php
+++ resources/views/packages/wait_payment_in_progress.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <h1>Please wait payment is in progress for status of subscription visit link below</h1>
-<a  href="{{ route('account.index') }}"></a>
-@endpush
+<a  href="{{ route('account.index') }}">asdasdasd</a>
+@endsection
