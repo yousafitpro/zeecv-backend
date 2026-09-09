90344c2cddffd5ef388c8ced7a639e63a52e203d
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-09 12:02:52 +0500

    x

--- app/Models/User.php
+++ app/Models/User.php
@@ -98,7 +98,7 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasOne(Visit::class)
                 ->where('ip_address',$this->signup_ip)
-                ->whereNotNull('utm_source')
+                // ->whereNotNull('utm_source')
                 ->oldest('created_at');
     }
     public function resume()
