df48ea03534455a317e9a284a9d7e20900bf6cf0
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-15 14:12:01 +0500

    Update social_signin_btns.blade.php

--- resources/views/modals/social_signin_btns.blade.php
+++ resources/views/modals/social_signin_btns.blade.php
@@ -73,13 +73,13 @@
             </a>
                 </div>
             </div>
-            {{-- <div class="col-md-6">
+            <div class="col-md-6">
                 <div class="social-btn linkedin-btn mt-2">
                   <a href="{{ route('facebook.auth') }}">
               <img src="{{ asset('assets/icons/facebook.png') }}" > Facebook
             </a>
                 </div>
-            </div> --}}
+            </div>
         </div>
       
 
