2ece9a58fd787fe4e48784d4cb6c02d81cf26b2c
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-09-19 13:41:12 +0500

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -91,6 +91,7 @@ class AuthController extends Controller
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
         ]);
+        }
     }
 
     /**
